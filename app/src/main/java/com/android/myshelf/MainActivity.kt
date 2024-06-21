a0904f9767065ce0d98e6ec1ec0dd0e66dd08a11
tastyapricot <notgarnet@gmail.com>
2024-06-21 13:11:22 +0300

    fix(example): remove unused field

--- app/src/main/java/com/android/myshelf/MainActivity.kt
+++ app/src/main/java/com/android/myshelf/MainActivity.kt
@@ -8,19 +8,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
-import com.android.myshelf.data.repositories.BookRepository
 import com.android.myshelf.presentation.shared.navigation.RootNavGraph
 import com.android.myshelf.presentation.shared.utils.isLight
 import com.android.myshelf.presentation.ui.theme.MyShelfTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var bookRepository: BookRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
